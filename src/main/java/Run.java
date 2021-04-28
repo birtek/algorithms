ed82334149ff187fbaf0e8201a7e0d1b68948bf1
birtek <barszy94@gmail.com>
2021-04-28 16:01:10 +0200

    Add second implementation of QuickSort

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,17 +1,16 @@
-import sortAlgorithms.ArithmeticString;
-import sortAlgorithms.BinarySearch;
-import sortAlgorithms.HeapSort;
-import sortAlgorithms.MyQuickSort;
+import sortAlgorithms.*;
 import sortAlgorithms.AlgorithmUtils.SortUtils;
+
+import java.util.Arrays;
 
 
 public class Run {
     public static void main(String[] args) {
         int[] array=new int[]{5,5,6,2,8,12,5,66,2,41,31,4677,0,33,55,667,876,-22};
 
-        MyQuickSort quicksort = new MyQuickSort();
+        //MyQuickSort quicksort = new MyQuickSort();
         System.out.println("Quicksort:");
-        quicksort.sort(array);
+        //quicksort.sort(array);
         SortUtils.printTable(array);
 
 
@@ -24,14 +23,17 @@ public class Run {
         int[] array3 = new int[]{};
         int number =2;
 
-        quicksort.sort(array3);
+     //   quicksort.sort(array3);
         System.out.println("Is number " + number + " is in table?");
         System.out.println(BinarySearch.binarySearch(array3,number));
 
         int[] array4=new int[]{5,5,6,2,8,12,5,66,2,41,31,4677,0,33,55,667,876,-22, -1000};
-        HeapSort.sort(array4);
-        System.out.println("Heap Sort");
-        SortUtils.printTable(array4);
-        }
+//        HeapSort.sort(array4);
+//        System.out.println("Heap Sort");
+//        SortUtils.printTable(array4);
+        QuickSortWithBorder.sort(array4);
+        System.out.println("Quick sort with Border");
+        System.out.println(Arrays.toString(array4));
+    }
     }
 
