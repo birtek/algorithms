2753192cb3a59d69aeebecd33ee66fb7c7b02dd5
birtek <barszy94@gmail.com>
2021-04-29 19:46:38 +0200

    Add second SelectSort with two for loop

--- src/main/java/sortAlgorithms/SelectSort.java
+++ src/main/java/sortAlgorithms/SelectSort.java
@@ -5,7 +5,7 @@ import sortAlgorithms.AlgorithmUtils.SortUtils;
 public class SelectSort {
 
     public static void sort(int[] arrayToSort) {
-        selectSort(arrayToSort);
+        selectSort2(arrayToSort);
     }
 
     private static void selectSort(int[] arrayToSort) {
@@ -14,10 +14,10 @@ public class SelectSort {
         int minValueIndex = 0;
 
         while (it <=n) {
-            int min = arrayToSort[it];
+            int minValueStart = arrayToSort[it];
             for (int i = it + 1; i < n; i++) {
-                if (min > arrayToSort[i]) {
-                    min = arrayToSort[i];
+                if (minValueStart > arrayToSort[i]) {
+                    minValueStart = arrayToSort[i];
                     minValueIndex = i;
                 }
             }
@@ -25,6 +25,23 @@ public class SelectSort {
                 SortUtils.swap(arrayToSort, it, minValueIndex);
             }
             it++;
+        }
+    }
+
+    private static void selectSort2(int[] arrayToSort){
+        int n = arrayToSort.length - 1;
+        int minIndex;
+
+        for (int i = 0;i < n; i++){
+            minIndex = i;
+            for (int j = i + 1;j <= n;j++){
+                if(arrayToSort[j]<arrayToSort[minIndex]){
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i){
+                SortUtils.swap(arrayToSort,i,minIndex);
+            }
         }
     }
 }
